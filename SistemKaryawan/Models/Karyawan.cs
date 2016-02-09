275a26b8567c73979ee6f629aa8e88d7e3411928
trioandianto <trio.andianto@basajans.com>
2016-02-09 14:57:01 +0700

    Kembali ke Jalan yang benar

---
+++ SistemKaryawan/Models/Karyawan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemKaryawan.Models
+{
+    abstract class Karyawan
+    {
+        public string divisi;
+
+        public int basicSalary;
+        public int tunnjanganTransportasi;
+
+    }
+}
